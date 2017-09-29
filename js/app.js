4b2c0830373999ab722eac628fe79c261958f33e
Alan Ciccotelli <alanciccotelli9@gmail.com>
2017-09-29 01:48:07 +0100

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,18 +1,39 @@
-/*
- * Create a list that holds all of your cards
- */
+const cardList = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
+const openCards = [];
+let moves = 0;
+
+function timer() {
+///////////////Timer///////////////////////////
+    let minutes = `00`;
+    let seconds = `00`;
+
+    let timerInterval = setInterval(function () {
+        let matchCounter = $('.match').length;
+        if (matchCounter === 16) {
+            clearInterval(timerInterval);
+        }
+        seconds++;
+        if (seconds < 10) {
+            seconds = `0${seconds}`;
+        }
+        if (seconds > 59) {
+            minutes++;
+            if (minutes < 10) {
+                minutes = `0${minutes}`;
+            }
+            seconds = `00`;
+        }
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+        $('.timer').text(`${minutes}:${seconds}`);
+
+    }, 1000);
+///////////////////////////////////////////////////
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +46,122 @@ function shuffle(array) {
     return array;
 }
 
+function buildCards() {
+    for (let round = 0; round < 2; round++) {
+        const mixedArray = shuffle(cardList);
+        mixedArray.forEach(function (card) {
+            $('.deck').append(` <li class="card">
+                <i class="fa ${card}" data-name="${card}"></i>
+            </li>`);
+        });
+    }
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+function starRating(moves) {
+    if (moves === 9) {
+        $('.three').css('color', 'black');
+    }
+    else if (moves >= 15) {
+        $('.two').css('color', 'black');
+    }
+
+}
+
+
+function cardCheck(card, array) {
+    // Comparing the 2 opened cards
+    if (array.length >= 2) {
+        moves++;
+        starRating(moves);
+        $('.moves').text(moves);
+        //disable clicks if it's more than 2 active.
+        $('.card').off();
+        if (card === array[0]) {
+            //Card Match!
+            $(`.${card}`).parent().addClass('match').removeClass('active');
+            array.length = 0;
+            //Checks for Game over
+            gameOver();
+            //Reset click events
+            $('.card').off();
+            enableClick();
+        } else {
+            //No match
+            for (item in array) {
+                $('.active').addClass('mismatch');
+            }
+            setTimeout(function () {
+                for (item in array) {
+                    $(`.${array[item]}`).parent().removeClass('open show mismatch active');
+                }
+                array.length = 0;
+                //Reset Click events
+                $('.card').off();
+                enableClick();
+            }, 500);
+
+        }
+    }
+}
+
+function enableClick() {
+    $('.card').on('click', function () {
+        $(this).addClass('open show active');
+        //Avoid clicking it again to find the same card.
+        $(this).off();
+        const card = $(this).children().attr('data-name');
+        openCards.push(card);
+        cardCheck(card, openCards);
+    });
+}
+
+function gameOver() {
+    let matchCounter = $('.match').length;
+    if (matchCounter === 16) {
+        if (moves <= 9) {
+            congrats = 'Amazing!';
+        }
+        else if (moves > 15) {
+            congrats = 'You can do better than this...';
+        }
+        else {
+            congrats = 'Good job, you almost got it!';
+        }
+        $('.container').append(`
+            <div class="game-over">
+                <div class="game-over-text">
+                    <h2>${congrats}</h2>
+                    <h3>You finished the game with ${moves} moves.</h3>
+                </div>
+            </div>`
+        );
+        $('.game-over').append(
+            `<div class="final-game">${$('.stars').html()}</div>
+            <div class="final-game">${$('.timer').html()}</div>
+            <div class="final-game">${$('.restart').html()}</div>`
+        );
+        setTimeout(function () {
+            $('.final-game').addClass('active');
+        }, 1500);
+        $('.deck, .score-panel').addClass('finished');
+        $('.restart').show();
+    }
+}
+
+function newGame() {
+    //Restart moves,timer,reset modals,stars,rebuild the cards.
+    moves = 0;
+    timer();
+    $('.game-over').remove();
+    $('.deck').children().remove();
+    $('.deck, .score-panel').removeClass('finished');
+    $('.restart').hide();
+    $('.fa-star').css('color', 'gold');
+    buildCards();
+    enableClick();
+    $('.moves').text(moves);
+    $('.timer').text('00:00');
+}
+
+//Init
+newGame();
